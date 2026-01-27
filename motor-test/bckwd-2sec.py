fad8130a7196158e0fa3a50762c563c90e7e203b
Mohamed Aiham Hussain <aihamhussain2001@gmail.com>
2026-01-27 15:04:31 +0800

    add .py files for bckwd, left, and right

---
+++ motor-test/bckwd-2sec.py
@@ -0,0 +1,37 @@
+from gpiozero import Motor, PWMOutputDevice
+from time import sleep
+from typing import Union
+
+ENA = 13  # Control right side motors; GPIO/BCM pin 13, Physical/Board pin 33
+ENB = 19  # Control left side motors;  GPIO/BCM pin 19, Physical/Board pin 35
+
+IN1 = 'BCM24'  # Controls the IN1 input on the L298N; GPIO/BCM pin 24, Physical/Board pin 18
+IN2 = 'BCM23'  # Controls the IN2 input on the L298N; GPIO/BCM pin 23, Physical/Board pin 16
+IN3 = 'BCM27'  # Controls the IN3 input on the L298N; GPIO/BCM pin 27, Physical/Board pin 13
+IN4 = 'BCM22'  # Controls the IN4 input on the L298N; GPIO/BCM pin 22, Physical/Board pin 15
+
+# Init gpiozero Motors
+left_dir = Motor(forward=IN1, backward=IN2)
+right_dir = Motor(forward=IN3, backward=IN4)
+
+# Init PWM control of motors
+right_pwm = PWMOutputDevice(ENA, frequency=1000)
+left_pwm = PWMOutputDevice(ENB, frequency=1000)
+
+
+def duty_cycle(input: Union[int, float]) -> None:
+    if input < 0:
+        left_pwm.value = 0
+        right_pwm.value = 0
+    elif input > 1:
+        left_pwm.value = 1
+        right_pwm.value = 1
+    else:
+        left_pwm.value = input
+        right_pwm.value = input
+
+
+duty_cycle(1)
+left_dir.backward()
+right_dir.backward()
+sleep(2)
