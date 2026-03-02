cdfd968fc7202144542a32e80d7679ff91e82b67
Mohamed Aiham Hussain <aihamhussain2001@gmail.com>
2026-03-02 13:06:50 +0800

    move frame processing to a function

--- PW2/camera-test.py
+++ PW2/camera-test.py
@@ -1,8 +1,25 @@
 from picamera2 import Picamera2
+from numpy.typing import NDArray
 import libcamera
 import cv2
 import numpy as np
 import time
+
+
+def process_frame(input_frame: NDArray) -> tuple[NDArray, NDArray]:
+    # Convert input frame to grayscale
+    processed_gray = cv2.cvtColor(input_frame, cv2.COLOR_RGB2GRAY)
+
+    # Apply Gaussian blur
+    processed_gray = cv2.GaussianBlur(processed_gray, (5, 5), 0)
+
+    # Apply an adaptive threshold to convert the image to
+    # pure black and pure white
+    adapt_thresh = cv2.adaptiveThreshold(processed_gray, 255,
+        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 199, 5)
+
+    return processed_gray, adapt_thresh
+
 
 picam2 = Picamera2()
 config = picam2.create_video_configuration(
@@ -19,21 +36,13 @@ print("Processing live feed. Press 'q' in the terminal window to quit.")
 
 try:
     while True:
-        # 1. Capture the frame as an array
+        # Capture a still frame from the camera
         frame = picam2.capture_array()
 
-        # 2. Grayscale Conversion: Simplifies processing [cite: 102]
-        # Note: Picamera2 RGB888 needs RGB2GRAY, not BGR2GRAY
-        processed = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        # Process frame using function
+        processed, adapt_thresh = process_frame(frame)
 
-        # 3. Noise Reduction: Gaussian filtering [cite: 103]
-        # This removes tiny details like floor scratches [cite: 104]
-        processed = cv2.GaussianBlur(processed, (5, 5), 0)
-
-        adapt_thresh = cv2.adaptiveThreshold(processed, 255,
-            cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 199, 5)
-
-        # 4. Display the results
+        # Display the different frames
         cv2.imshow('Original', frame)
         cv2.imshow('Pre-Processed (Gray + Blur)', processed)
         cv2.imshow('Thresholded', adapt_thresh)
