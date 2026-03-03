337d5573382b759c12638003a92443e327d7bc88
Mohamed Aiham Hussain <aihamhussain2001@gmail.com>
2026-03-03 10:18:42 +0800

    Revert multiple commits to camera-test.py
    Revert all new image processing commits in preparation for moving said
    code to a dedicated python file for testing further improvements to
    image processing for the line
    
    Revert "add pre-processing to improve contrast"
    This reverts commit d1de963b959d6c671debf40effd31cdfe628f8f3.
    
    Revert "invert image when thresholding and not afterwards"
    This reverts commit 044964609ef69d1bf7403029f9e9ad46e88244cc.
    
    Revert "improve thresholding"
    This reverts commit adb2af5bcb3bd3776e848cd82dbd852113c9cb72.
    
    Revert "switch to gaussian adaptiveThreshold"
    This reverts commit 4de4a417f8bbd080fb332cd738a7b1720bab27a0.

--- PW2/camera-test.py
+++ PW2/camera-test.py
@@ -1,40 +1,23 @@
 from picamera2 import Picamera2
 from numpy.typing import NDArray
-import numpy as np
 import libcamera
 import cv2
 import time
 
 
-def process_frame(input_frame: NDArray) -> tuple[NDArray, NDArray, NDArray]:
-    # Apply CLAHE on V channel to inprove contrast
-    h, s, v = cv2.split(cv2.cvtColor(input_frame, cv2.COLOR_RGB2HSV))
-    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
-    frame_eq = cv2.cvtColor(
-        cv2.merge([h, s, clahe.apply(v)]), cv2.COLOR_HSV2RGB)
-
+def process_frame(input_frame: NDArray) -> tuple[NDArray, NDArray]:
     # Convert input frame to grayscale
-    # processed_gray = cv2.cvtColor(input_frame, cv2.COLOR_RGB2GRAY)
-    processed_gray = cv2.cvtColor(frame_eq, cv2.COLOR_RGB2GRAY)
+    processed_gray = cv2.cvtColor(input_frame, cv2.COLOR_RGB2GRAY)
 
     # Apply Gaussian blur
-    processed_gray = cv2.GaussianBlur(processed_gray, (7, 7), 0)
+    processed_gray = cv2.GaussianBlur(processed_gray, (5, 5), 0)
 
     # Apply an adaptive threshold to convert the image to
     # pure black and pure white
-    thresh = cv2.adaptiveThreshold(processed_gray, 255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 45, 5)
+    adapt_thresh = cv2.adaptiveThreshold(processed_gray, 255,
+        cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 199, 5)
 
-    # Apply Otsu's Binarization to normal thresholding
-    # _, thresh = cv2.threshold(processed_gray, 0, 255,
-    #     cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    # thresh = cv2.bitwise_not(thresh)  # invert colours for morphology operations
-    kernel = np.ones((7, 7), np.uint8)  # for morphology operations
-    thresh = cv2.erode(thresh, kernel, iterations=1)
-    thresh = cv2.bitwise_not(thresh)  # make the colours normal again
-
-    return frame_eq, processed_gray, thresh
+    return processed_gray, adapt_thresh
 
 
 picam2 = Picamera2()
@@ -56,11 +39,10 @@ try:
         frame = picam2.capture_array()
 
         # Process frame using function
-        frame_eq, processed, adapt_thresh = process_frame(frame)
+        processed, adapt_thresh = process_frame(frame)
 
         # Display the different frames
         cv2.imshow('Original', frame)
-        cv2.imshow('Pre-Processed (Contrast Inc)', frame_eq)
         cv2.imshow('Pre-Processed (Gray + Blur)', processed)
         cv2.imshow('Thresholded', adapt_thresh)
 
