f0d2c1c799af38e8c20568c4379d2f087c4d0426
Mohamed Aiham Hussain <aihamhussain2001@gmail.com>
2026-04-09 10:33:30 +0800

    assume largest contour is arrow in arw-dtct
    this is only possible because we're filtering out the black line

--- PW3/arrow-detection.py
+++ PW3/arrow-detection.py
@@ -93,34 +93,33 @@ try:
         # Draw contours onto frame ROI
         cv2.drawContours(frame_roi_w_contours, contours, -1, (0, 255, 0), 3)
 
+        # Assume the largest contour is the arrow
+        # Assumption is only possible due to the fact that the black line is being filtered out
+        arrow_cnt = max(contours, key=cv2.contourArea)
         detected_arrow = 'None'
         arrow_area = 0
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if 1000 < area < 15600:
-                hull = cv2.convexHull(cnt)
-                solidity = float(area) / cv2.contourArea(hull) if cv2.contourArea(hull) > 0 else 0
-                if 0.52 <= solidity <= 0.75:
-                    x, y, w, h = cv2.boundingRect(cnt)
-                    M = cv2.moments(cnt)
-                    if M["m00"] > 0:
-                        cX, cY = int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])
-                        cv2.circle(frame_roi_w_contours, (cX, cY), 4, (255, 0, 0), 4)
-                        bX, bY = x + (w / 2), y + (h / 2)
-                        cv2.circle(frame_roi_w_contours, (int(bX), int(bY)), 4, (0, 255, 0), 4)
-                        if abs(cX - bX) > abs(cY - bY):
-                            detected_arrow = "Arrow Right" if cX > bX else "Arrow Left"
-                            arrow_area = area
-                        else:
-                            detected_arrow = "Arrow Down" if cY > bY else "Arrow Up"
-                            arrow_area = area
-                        break
-                    else:
-                        detected_arrow = 'None'
+
+        area = cv2.contourArea(arrow_cnt)
+        hull = cv2.convexHull(arrow_cnt)
+        solidity = float(area) / cv2.contourArea(hull) if cv2.contourArea(hull) > 0 else 0
+        if 0.52 <= solidity <= 0.75:
+            x, y, w, h = cv2.boundingRect(arrow_cnt)
+            M = cv2.moments(arrow_cnt)
+            if M["m00"] > 0:
+                cX, cY = int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])
+                cv2.circle(frame_roi_w_contours, (cX, cY), 4, (255, 0, 0), 4)
+                bX, bY = x + (w / 2), y + (h / 2)
+                cv2.circle(frame_roi_w_contours, (int(bX), int(bY)), 4, (0, 255, 0), 4)
+                if abs(cX - bX) > abs(cY - bY):
+                    detected_arrow = "Arrow Right" if cX > bX else "Arrow Left"
+                    arrow_area = cv2.contourArea(arrow_cnt)
                 else:
-                    detected_arrow = 'None'
+                    detected_arrow = "Arrow Down" if cY > bY else "Arrow Up"
+                    arrow_area = cv2.contourArea(arrow_cnt)
             else:
                 detected_arrow = 'None'
+        else:
+            detected_arrow = 'None'
         print(f'Detected Arrow : {detected_arrow} || Arrow Area : {arrow_area:.2f}')
 
         # Display the different frames
@@ -128,7 +127,7 @@ try:
         # cv2.imshow('Pre-Processed (Gray + Blur)', processed)
         # cv2.imshow('Thresholded', thresh)
         # cv2.imshow('Orignal ROI', frame_roi)
-        # cv2.imshow('Thresholded ROI', thresh_roi)
+        cv2.imshow('Thresholded ROI', thresh_roi)
         # cv2.imshow('Black Mask', black_mask)
         cv2.imshow(
             'ROI w/ contours',
