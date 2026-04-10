bb59e812fb2a0d60e3e199c2b8b7ee2cc3f70396
Mohamed Aiham Hussain <aihamhussain2001@gmail.com>
2026-04-10 13:26:04 +0800

    port improved line following from noarrow code

--- PW3/line-following.py
+++ PW3/line-following.py
@@ -66,7 +66,7 @@ def process_frame(input_frame: NDArray[np.uint8]) -> tuple[NDArray[np.uint8], ND
     processed_gray = cv2.GaussianBlur(processed_gray, (9, 9), 0)
 
     # Just use normal thresholding
-    _, thresh = cv2.threshold(processed_gray, 160, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(processed_gray, 150, 255, cv2.THRESH_BINARY_INV)
 
     # Apply morphology operations
     thresh = process_frame_morphology_ops(thresh)
@@ -84,7 +84,7 @@ def process_frame_morphology_ops(input_thresh):
 
 def thresh2maincontour(threshhold):
     # Get contours from threshhold
-    contours, hierarchy = cv2.findContours(threshhold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(threshhold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Find and return main contour
     ## modified from https://github.com/tprlab/pitanq-dev
@@ -103,57 +103,53 @@ def calc_centroid(main_cnt):
     return centroid_x, centroid_y
 
 
-def detect_colored_line(raw_frame, colour):
+def detect_coloured_line(raw_frame, colour):
+
+    def gen_masks(hsv_frame, colour):
+        ret_mask = None
+        if colour == 'RED':
+            red_toplow, red_tophigh = COLOUR_RANGES['red']['top']
+            red_btmlow, red_btmhigh = COLOUR_RANGES['red']['btm']
+            red_masktop = cv2.inRange(hsv_frame, np.array(red_toplow), np.array(red_tophigh))
+            red_maskbtm = cv2.inRange(hsv_frame, np.array(red_btmlow), np.array(red_btmhigh))
+            red_mask = cv2.bitwise_or(red_maskbtm, red_masktop)
+            ret_mask = red_mask.copy()
+        elif colour == 'YELLOW':
+            ylw_low, ylw_high = COLOUR_RANGES['yellow']
+            ylw_mask = cv2.inRange(hsv, np.array(ylw_low), np.array(ylw_high))
+            ret_mask = ylw_mask.copy()
+        else:
+            raise ValueError(f'Unknown value for colour : [{colour}]')
+        if ret_mask is not None:
+            ret_mask = process_frame_morphology_ops(ret_mask)
+            return ret_mask
+        else:
+            raise ValueError('ret_mask cannot be None')
+
     hsv = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2HSV)
-    if colour == "black":
-        return False, None, None
+    colour_mask = gen_masks(hsv, colour)
+    colour_present = np.count_nonzero(colour_mask) > 0
 
-    ylw_low, ylw_high = COLOUR_RANGES['yellow']
-    ylw_mask = cv2.inRange(hsv, np.array(ylw_low), np.array(ylw_high))
-
-    red_toplow, red_tophigh = COLOUR_RANGES['red']['top']
-    red_btmlow, red_btmhigh = COLOUR_RANGES['red']['btm']
-    red_masktop = cv2.inRange(hsv, np.array(red_toplow), np.array(red_tophigh))
-    red_maskbtm = cv2.inRange(hsv, np.array(red_btmlow), np.array(red_btmhigh))
-    red_mask = cv2.bitwise_or(red_maskbtm, red_masktop)
-
-    match colour:
-        case "yellow":
-            accept_mask = process_frame_morphology_ops(ylw_mask)
-            reject_mask = process_frame_morphology_ops(red_mask)
-            colour_present = np.count_nonzero(accept_mask) > 0
-            return bool(colour_present), accept_mask, reject_mask
-        case "red":
-            accept_mask = process_frame_morphology_ops(red_mask)
-            reject_mask = process_frame_morphology_ops(ylw_mask)
-            colour_present = np.count_nonzero(accept_mask) > 0
-            return bool(colour_present), accept_mask, reject_mask
-        case "both":
-            both_mask = cv2.bitwise_or(ylw_mask, red_mask)
-            both_mask = process_frame_morphology_ops(both_mask)
-            colour_present = np.count_nonzero(both_mask) > 0
-            return bool(colour_present), both_mask, None
-        case _:
-            raise ValueError(f"Unknown value for input value \"colour\" [{colour}]")
+    return bool(colour_present), colour_mask
 
 
 def linefollowing_colourchoice() -> str:
     options = {
-        '1': ('black', 'Black line only'),
-        '2': ('red', 'Red line or black line'),
-        '3': ('yellow', 'Yellow line or black line'),
-        '4': ('both', 'Red or yellow or black line'),
+        '1': ('black', 'Black line only', None),
+        '2': ('red', 'Red line or black line', ['RED']),
+        '3': ('yellow', 'Yellow line or black line', ['YELLOW']),
+        '4': ('both', 'Red or yellow or black line', ['RED', 'YELLOW']),
     }
     print("Select line colour(s) to follow.\nOptions:")
-    for key, (_, desc) in options.items():
+    for key, (_, desc, _) in options.items():
         print(f"{key} : {desc}")
 
     while True:
         raw = input("\nEnter choice [1-4]: ").strip()
         if raw in options:
-            color, desc = options[raw]
+            color, desc, retval = options[raw]
             print(f"Line following colour choice set to: [{desc}]\n")
-            return color
+            return retval
         print(f"  Invalid choice {raw!r} — please enter 1, 2, 3, or 4.")
 
 
@@ -253,7 +249,7 @@ pid = ownPID(0.5, 0.01, 0.05)
 BASE_SPEED = 0.40
 MIN_SPEED = 0.30
 
-global COLOUR_RANGES, PRIORITY_COLOUR
+global COLOUR_RANGES, PRIORITY_COLOURS
 COLOUR_RANGES = {
     "red": {
     "top": [(170, 150, 100), (180, 255, 255)],
@@ -261,7 +257,7 @@ COLOUR_RANGES = {
     },
     "yellow": [(20, 190, 190), (30, 255, 255)],
 }
-PRIORITY_COLOUR = linefollowing_colourchoice()
+PRIORITY_COLOURS = linefollowing_colourchoice()
 
 picam2 = Picamera2()
 config = picam2.create_video_configuration(
@@ -292,10 +288,13 @@ time.sleep(0.2)
 
 start_time = last_time = last_arrow_detect_time = time.time()
 
-global followed_colour, ini_colour_dir, colour_dir_check
+global current_colour, followed_colour, colour_dir_check, ini_colour_dir
+current_colour = None
 followed_colour = False
 colour_dir_check = False
 ini_colour_dir = None
+colour_present = False
+accept_mask = None
 
 try:
     while True:
@@ -318,23 +317,18 @@ try:
         thresh_roi = thresh[int(height *
             (frame_discard_percentage - frame_discard_offset)):int(height * (1 - frame_discard_offset)):]
 
-        colour_present, accept_mask, reject_mask = detect_colored_line(frame_roi, PRIORITY_COLOUR)
-
-        if colour_present:
-            followed_colour = True
-            thresh_roi = accept_mask.copy()  # type: ignore
-            col_main_cent_x, _ = calc_centroid(thresh2maincontour(thresh_roi))
-            if colour_dir_check is False:
-                ini_colour_dir = 'Right' if (col_main_cent_x > (cam_size_x / 2)) else 'Left'
-                colour_dir_check = True
-        if reject_mask is not None:
-            thresh_roi = cv2.bitwise_xor(thresh_roi, reject_mask)
-
-        if (now - last_time) > 2:
-            last_time = time.time()
-            print(
-                f"Clr : [{colour_present}] || Flwd : [{followed_colour}] || Dir Chk : {colour_dir_check} || Dir : [{ini_colour_dir}]"
-            )
+        if PRIORITY_COLOURS is not None:
+            for colour in PRIORITY_COLOURS:
+                colour_present, accept_mask = detect_coloured_line(frame_roi, colour)
+                if colour_present is True:
+                    current_colour = colour
+                    followed_colour = True
+                    thresh_roi = accept_mask.copy()  # type: ignore
+                    col_main_cent_x, _ = calc_centroid(thresh2maincontour(thresh_roi))
+                    if colour_dir_check is False:
+                        ini_colour_dir = 'Right' if (col_main_cent_x > (cam_size_x / 2)) else 'Left'
+                        colour_dir_check = True
+                    break
 
         thresh_roi = process_frame_morphology_ops(thresh_roi)
 
@@ -370,9 +364,14 @@ try:
                 left_dir.forward()
                 right_dir.forward()
 
-                move_sec = 1
-                print(f'Moving for {move_sec}sec')
-                time.sleep(1)
+                if current_colour == 'RED':
+                    move_sec = 1
+                    print(f'[RED LINE] Moving for {move_sec}sec')
+                    current_colour = None
+                    time.sleep(move_sec)
+                else:
+                    current_colour = None
+
             else:
                 cent_x, cent_y = calc_centroid(main_contour)
                 pid_out = pid.update(cent_x, dt)
